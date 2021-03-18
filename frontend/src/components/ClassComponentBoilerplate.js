232072265a98653737ef38a162048a710050db73
Kenneth Mui <klmui@wisc.edu>
2021-03-18 02:05:45 -0500

    add component boilerplate

---
+++ frontend/src/components/ClassComponentBoilerplate.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ClassComponentBoilerplate extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getName(){
+    return "ClassComponentBoilerplate"
+  }
+
+  render() {
+    return (
+      <h1>
+        Hello, my name is <strong>{this.getName()}</strong>.
+      </h1>
+    );
+  }
+}
+
+export default ClassComponentBoilerplate;
