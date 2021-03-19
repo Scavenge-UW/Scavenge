89935ff767f640af83c64f4178288b9fb68eeabb
Ilkyu Ju <ju.ilkyu@gmail.com>
2021-03-19 02:22:14 -0500

    Implement basic design & layout for InventoryView

---
+++ frontend/src/components/FoodItemCard.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { VscCircleFilled } from "react-icons/vsc";
+
+import '../css/common.css'
+
+class FoodItemCart extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  isInStock(){
+    return this.props.foodItem.quantity > 0
+  }
+
+  showStockInfo() {
+    if (this.isInStock()){
+      return (
+        <>
+          In Stock 
+          <VscCircleFilled color="green" />
+        </>
+      )
+    } else {
+      return (
+        <>
+          Out of Stock 
+          <VscCircleFilled color="red" />
+        </>
+      )
+    }
+  }
+  
+  render() {
+    const { name } = this.props.foodItem;
+    return (
+      <Card className="food-item">
+        <Card.Body>
+          <Card.Title className="mb-4">
+            <Row className="justify-content-between align-items-center">
+              <Col className="text-left">
+                {name}
+              </Col>
+              <Col className="text-right">
+                {/* Show if Item is in stock */}
+                {this.showStockInfo()}
+              </Col>
+            </Row>
+          </Card.Title>
+          <hr />
+            <Row>
+              <Col>
+                <Form>
+                  <Form.Group controlId="formQuantity">
+                    <Form.Label>Enter quantity in stock</Form.Label>
+                    <Form.Control type="number" placeholder="Quantity" />
+                  </Form.Group>
+                </Form>
+              </Col>
+              <Col>
+                {/* Empty label is for aligning the input and the button */}
+                <Form.Label>&nbsp;</Form.Label> 
+                <Button block>Update</Button>
+              </Col>
+            </Row>
+            <Row>
+              <Button className="ml-3 mr-3" block variant="outline-danger">Remove</Button>
+            </Row>
+          </Card.Body>
+      </Card>
+    );
+  }
+}
+
+export default FoodItemCart;
---
+++ frontend/src/components/InventoryView.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+import FoodItemCard from './FoodItemCard';
+
+class InventoryView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Dummy Food Items
+      foodItems: [
+        {
+          food_id: 1,
+          name: "Apple",
+          quantity: 123,
+        },
+        {
+          food_id: 2,
+          name: "Banana",
+          quantity: 0,
+        },
+      ]
+    }
+  }
+
+  // Returns a list of <FoodItemCard> instances 
+  // based on the list of food items fetched from the API
+  getFoodItemCards(){
+    let foodItemCards = [];
+    for (const foodItem of this.state.foodItems) { // TODO: Change to props when API is implemented
+      foodItemCards.push(
+        <FoodItemCard
+          key={foodItem.id}
+          foodItem={foodItem}
+        />
+      )
+    } 
+    return foodItemCards;
+  }
+  
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center">
+          <h2>Current Inventory</h2>
+        </Row>
+        <Row className="justify-content-end">
+          <Button>Add item</Button>
+        </Row>
+        <Row className="justify-content-center">
+          {this.getFoodItemCards()}
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default InventoryView;
