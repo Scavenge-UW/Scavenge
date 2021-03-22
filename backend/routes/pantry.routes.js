687ac8ccd2f137cabe035fb1fb997146f8a7faac
s-j-cunningham <sjcunningham@wisc.edu>
2021-03-22 10:20:45 -0500

    add food endpoint

--- backend/routes/pantry.routes.js
+++ backend/routes/pantry.routes.js
@@ -6,4 +6,7 @@ const { getPantryDetailAction } = require("../controllers/pantry.controllers.js"
 // Get pantry details
 router.get('/pantries/:pantry_id', getPantryDetailAction);
 
+// Add a food to inventory
+router.get('/pantries/:pantry_id/:food_id', getPantryDetailAction);
+
 module.exports = router; // We need this at the end of every route file
