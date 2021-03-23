621ab5feec8c0e28baa213a08d1fad3c8ac6ef39
s-j-cunningham <sjcunningham@wisc.edu>
2021-03-23 17:46:08 -0500

    corrected new routes to put rather than post

--- backend/routes/pantry.routes.js
+++ backend/routes/pantry.routes.js
@@ -22,12 +22,12 @@ router.put('/pantries/:pantry_id/:food_id', pantryUpdateInventoryAction);
 router.post('/pantries/:pantry_id/:food_id', pantryUpdateInventoryAction);
 
 // Mark a reservation as picked up
-router.post('/pantries/:pantry_id/reservations/:reservation_id', completeReservationAction);
+router.put('/pantries/:pantry_id/reservations/:reservation_id', completeReservationAction);
 
 // Mark a reservation as approved
-router.post('/pantries/:pantry_id/reservations/approve/:reservation_id', approveReservationAction);
+router.put('/pantries/:pantry_id/reservations/approve/:reservation_id', approveReservationAction);
 
 // Mark a reservation as cancelled
-router.post('/pantries/:pantry_id/reservations/cancel/:reservation_id', cancelReservationAction);
+router.put('/pantries/:pantry_id/reservations/cancel/:reservation_id', cancelReservationAction);
 
 module.exports = router; // We need this at the end of every route file
