4c99522dbf913fcf28f88abd3e074418b1563c57
Kenneth Mui <klmui@wisc.edu>
2021-03-25 02:15:46 -0500

    fuzzy search for listview pantries

--- frontend/src/components/ListView.js
+++ frontend/src/components/ListView.js
@@ -6,27 +6,71 @@ import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 // Redux
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchPantries } from '../actions/pantryActions';
 
+import Fuse from "fuse.js";
+
 import '../css/ListView.css';
+import { ThemeConsumer } from 'react-bootstrap/esm/ThemeProvider';
 
 class ListView extends Component {
   constructor(props) {
-    super(props);  }
+    super(props);  
+    this.state = {
+      matchedPantries: null
+    }
+    this.searchData = this.searchData.bind(this);
+  }
 
   componentDidMount() {
     this.props.fetchPantries();
   }
 
+  searchData(pattern) {
+    if (this.props.pantries.result !== null && typeof (this.props.pantries.result) !== "undefined") {
+      let pantries = [];
+      console.log(this.props.pantries);
+      Object.values(this.props.pantries.result).map((pantry) => {
+        pantries.push(pantry);
+      });
+  
+      if (!pattern) {
+        console.log("HRE1")
+        this.setState({ matchedPantries: pantries });
+        return;
+      }
+  
+      // Perform fuzzy search for pantries
+      const fuse = new Fuse(pantries, {
+        keys: ["name", "city", "state", "phone_number", "address", "zip"],
+      });
+      const result = fuse.search(pattern);
+  
+      if (!result.length) {
+        // No matches
+        console.log("HRE3");
+        this.setState({ matchedPantries: [] });
+      } else {
+        pantries = [];
+        result.forEach(({item}) => {
+          pantries.push(item);
+        });
+        this.setState({ matchedPantries: pantries });
+        console.log(this.state.matchedPantries);
+      }
+    }
+  }
     
   render() {
     let cards = [];
-    if (typeof this.props.pantries.result !== "undefined") {
-      console.log(this.props.pantries.result);
-      Object.values(this.props.pantries.result).map((pantry) => {
+    if (this.state.matchedPantries !== null && typeof this.state.matchedPantries !== "undefined") {
+      this.state.matchedPantries.map((pantry) => {
         cards.push(
             <Card>
               <Card.Header className="text-center">
@@ -46,11 +90,24 @@ class ListView extends Component {
             </Card>
         );
       }); 
+    } else {
+      this.searchData(""); // Populate matchedPantries
     }
 
     return (
       <div>
         <h6 className="text-center">All Food Pantries</h6>
+        <div className="Search">
+          <span className="SearchSpan">
+            <FontAwesomeIcon className="pin" icon={faSearch}/>
+          </span>
+          <input
+            className="SearchInput"
+            type="text"
+            onChange={(e) => this.searchData(e.target.value)}
+            placeholder="UW Food Shed"
+          />
+        </div>
         <Accordion className="listGroup">
           {cards}
         </Accordion>
