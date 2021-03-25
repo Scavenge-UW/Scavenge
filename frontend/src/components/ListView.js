5324fdb5158a7aac0b0c43c53f7059002fcb1942
Kenneth Mui <klmui@wisc.edu>
2021-03-25 00:17:48 -0500

    switch list to accordion and fill it with api data

--- frontend/src/components/ListView.js
+++ frontend/src/components/ListView.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 // Redux
 import PropTypes from 'prop-types';
@@ -13,63 +15,45 @@ import '../css/ListView.css';
 
 class ListView extends Component {
   constructor(props) {
-    super(props);
-  }
+    super(props);  }
 
   componentDidMount() {
     this.props.fetchPantries();
   }
 
-  
-  // render() {
-  //   return (
-  //     <div className="Container">
-  //       {this.props.pantries.result.map((pantry) => {
-          
-  //       })}
-  //     </div>
-  //   );
-  // }
-
     
   render() {
+    let cards = [];
+    if (typeof this.props.pantries.result !== "undefined") {
+      console.log(this.props.pantries.result);
+      Object.values(this.props.pantries.result).map((pantry) => {
+        cards.push(
+            <Card>
+              <Card.Header className="text-center">
+                <Accordion.Toggle as={Button} variant="link" eventKey={pantry.pantry_id}>
+                  {pantry.name}
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey={pantry.pantry_id}>
+                <Card.Body>
+                  <strong>Address: </strong>{pantry.address}, {pantry.city}, {pantry.state}, {pantry.zip}
+                  <br></br>
+                  <strong>Website: </strong>{pantry.website}
+                  <br></br>
+                  <strong>Phone: </strong>{pantry.phone_number}
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+        );
+      }); 
+    }
+
     return (
       <div>
         <h6 className="text-center">All Food Pantries</h6>
-        <ListGroup className="listGroup">
-          <ListGroup.Item action>
-            Link 1
-          </ListGroup.Item>
-          <ListGroup.Item action>
-            Link 2
-          </ListGroup.Item>
-          <ListGroup.Item action>
-            This one is a button
-          </ListGroup.Item>
-          <ListGroup.Item action>
-          This one is a button
-        </ListGroup.Item> <ListGroup.Item action>
-        This one is a button
-      </ListGroup.Item> <ListGroup.Item action>
-      This one is a button
-    </ListGroup.Item> <ListGroup.Item action>
-    This one is a button
-  </ListGroup.Item> <ListGroup.Item action>
-  This one is a button
-</ListGroup.Item> <ListGroup.Item action>
-This one is a button
-</ListGroup.Item> <ListGroup.Item action>
-This one is a button
-</ListGroup.Item> <ListGroup.Item action>
-This one is a button
-</ListGroup.Item> <ListGroup.Item action>
-This one is a button
-</ListGroup.Item> <ListGroup.Item action>
-This one is a button
-</ListGroup.Item> <ListGroup.Item action>
-This one is a button
-</ListGroup.Item>
-        </ListGroup>
+        <Accordion className="listGroup">
+          {cards}
+        </Accordion>
       </div>
     );
   }
