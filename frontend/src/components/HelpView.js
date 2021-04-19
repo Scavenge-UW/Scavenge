3dd09f2bc044bf14ac1e1aedb231473e2e2626c7
Jason Sutanto <jsutanto2@wisc.edu>
2021-04-19 09:05:32 -0500

    added styling to help page

--- frontend/src/components/HelpView.js
+++ frontend/src/components/HelpView.js
@@ -3,19 +3,19 @@ import React, { Component } from 'react';
 export default class HelpView extends Component {
     render() {
         return (
-            <div className="container" style={{background: "#FFF", height: "100vh"}}>
-               <h1 className="text-center" style={{paddingTop:"20px"}}>Help Page</h1>
+            <div className="container" style={{background: "#FFF", height: "150vh"}}>
+               <h1 className="text-center" style={{paddingTop:"20px", fontFamily:"courier"}}>Help Page</h1>
                
                <div className="container mt-4">
-                <h5>Frequently Asked Questions</h5>
-                <ul>
+                <h5 style={{fontFamily:"Courier"}}>Frequently Asked Questions</h5>
+                <ul style={{fontFamily:"Courier"}}>
                     <li><em>How do I manage a pantry?</em> Ask Kenny to email you the information for a pantry manager.</li>
-                    <li><em>How do I edit my pantry's infomation?</em>Login as a pantry staff or admin and go to "Manage Pantry". Scroll to the bottom to edit your pantry's description and hours (we know it can be a lot of scrolling unfortunately and are currently addressing this issue).</li>
+                    <li><em>How do I edit my pantry's infomation?</em> Login as a pantry staff or admin and go to "Manage Pantry". Scroll to the bottom to edit your pantry's description and hours (we know it can be a lot of scrolling unfortunately and are currently addressing this issue).</li>
                     <li><em>How do I make a pantry?</em> Email your pantry information to Kenny and he will submit it once it has been approved.</li>
                 </ul>
             
-                <h5>Contact Info</h5>
-                <ul>
+                <h5 style={{fontFamily:"Courier"}}>Contact Info</h5>
+                <ul style={{fontFamily:"Courier"}}>
                     <li>Kenny: Project Leader / Backend Lead / Frontend Developer</li>
                     <ul>
                         <li>Email: klmui@wisc.edu@wisc.edu</li>
@@ -47,8 +47,8 @@ export default class HelpView extends Component {
                     </ul>
                 </ul>
 
-                <h5>Current Features</h5>
-                <ul>
+                <h5 style={{fontFamily:"Courier"}}>Current Features</h5>
+                <ul style={{fontFamily:"Courier"}}>
                     <li>User: Search for nearby pantries</li>
                     <li>User: See what's in stock for each pantry by using the search form or clicking a pantry</li>
                     <li>User: Make and cancel reservations</li>
@@ -59,8 +59,8 @@ export default class HelpView extends Component {
                     <li>Admin: Update food quantities</li>
                 </ul>
 
-                <h5>Upcoming Features</h5>
-                <ul>
+                <h5 style={{fontFamily:"Courier"}}>Upcoming Features</h5>
+                <ul style={{fontFamily:"Courier"}}>
                     <li>Wishlist items</li>
                     <li>Email notifications when a wishlisted item comes back in stock and when their reservation gets approved</li>
                     <li>Real-time inventory</li>
