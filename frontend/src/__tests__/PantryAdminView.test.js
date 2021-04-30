b5ad9b5d25e0b561c90f35a6a2b3b15d206be001
Ilkyu Ju <ju.ilkyu@gmail.com>
2021-04-30 16:02:25 -0500

    test: fix PantryAdminView test failure

--- frontend/src/__tests__/PantryAdminView.test.js
+++ frontend/src/__tests__/PantryAdminView.test.js
@@ -6,6 +6,11 @@ import PantryAdminView from "../components/components_admin/PantryAdminView";
 import "../setupTests";
 import pantryDetail from "../__mocks__/pantryDetailMock";
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => jest.fn().mockReturnValue({ pantry_id: 1 }),
+}));
+
 describe("PantryAdminView tests", () => {
   const wrapper1 = shallow(<PantryAdminView />);
 
